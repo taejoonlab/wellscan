49ee8a997f957b751393ac8011e0e3423888a131
Taejoon Kwon <linusben@gmail.com>
2014-03-11 14:45:48 -0500

    Generalize bash script

--- plate-to-96.sh
+++ plate-to-96.sh
@@ -2,9 +2,9 @@
 for TIF in $(ls *P?.tif)
 do
   echo $TIF
-  OUT=${TIF/.tif/_96.tif}
-  if [ ! -e $OUT ]; then
-    ./plate-to-96.py $TIF
+  GRID=${TIF/.tif/.grid.jpg}
+  if [ ! -e $GRID ]; then
+    $HOME/git/wellscan/plate-to-96.py $TIF
   else
     echo "SKIP "$TIF
   fi
