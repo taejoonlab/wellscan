69ab0b37bc60bfd3c2379553134ceef316949821
Taejoon Kwon <linusben@gmail.com>
2014-10-01 10:00:50 -0500

    Shell script to make individual well image.

---
+++ plate-to-96well.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#export PYTHONPATH="/home1/00992/linusben/local/lib64/python2.6/site-packages/"
+
+for TIF in $(ls *.tif)
+do
+  echo $TIF
+  OUT=${TIF/.tif/.grid.jpg}
+  if [ ! -e $OUT ]; then
+    $HOME/git/wellscan/plate-to-well.py $TIF 96
+  else
+    echo "SKIP "$TIF
+  fi
+done
